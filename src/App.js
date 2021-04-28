d64770f10004d949bf4c8c5a1ca5ca21b149b79e
Zach Frohardt <zfrohardt@gmail.com>
2021-04-28 11:13:26 -0700

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
 // create your App component here
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render() {
+        return null;
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json()).then(console.log);
+    }
+}
